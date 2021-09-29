4cceecde41a91399181ef9bad521414a662a12ee
Jinn30 <karenthjinneth2002@gmail.com>
2021-09-29 16:02:42 -0500

    Agregado nuevo archivo, homework.js

---
+++ homework.js
@@ -0,0 +1,14 @@
+// La variable "string", contiene lineas de caráctes, en su defecto letras (necesita comillas dobles o simples).
+var letras = "Hola";
+
+//La variable numérica, que puede contener cualquier número y no necesita comillas.
+var numero = 45;
+
+//Las VARIABLES BOOLEANAS se expresan como verdadero o falso.
+var bool1 = true; //VERDADERO
+var bool2 = false; //FALSO
+
+//OPERACIONES MATEMATICAS
+
+var resta = 12 - 4
+console.log = resta
